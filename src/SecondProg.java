49f75586ee3ab362f62560a774128130c99c989c
Catv1l <70894585+Catv1l@users.noreply.github.com>
2020-11-21 01:09:52 +0300

    8 задач

--- src/SecondProg.java
+++ src/SecondProg.java
@@ -5,12 +5,13 @@ public class SecondProg {
     {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("1: ");
+
+        System.out.println("1: ");
         String str1 = in.next();
         int a1 = in.nextInt();
         repeat(str1,a1);
 
-        System.out.print("2: ");
+        System.out.println("2: ");
         int k2 = in.nextInt();
         int mas2 []= new int [k2];
         for (int i=0;i<k2;i++)
@@ -19,7 +20,7 @@ public class SecondProg {
         }
         differenceMaxMin(mas2);
 
-        System.out.print("3: ");
+        System.out.println("3: ");
         int k3 = in.nextInt();
         int mas3 []= new int [k3];
         for (int i=0;i<k3;i++)
@@ -28,7 +29,7 @@ public class SecondProg {
         }
         isAvgWhole(mas3);
 
-        System.out.print("4: ");
+        System.out.println("4: ");
         int k4 = in.nextInt();
         int mas4 []= new int [k4];
         for (int i=0;i<k4;i++)
@@ -37,15 +38,35 @@ public class SecondProg {
         }
         cumulativeSum(mas4);
 
-        System.out.print("5: ");
+        System.out.println("5: ");
         String str5 = in.next();
         getDecimalPlaces(str5);
 
-        System.out.print("6: ");
+        System.out.println("6: ");
         int k6 = in.nextInt();
         fibonacci(k6);
 
+        System.out.println("7: ");
+        String str7 = in.next();
+        isValid(str7);
 
+        System.out.println("8: ");
+        String str8_1 = in.next();
+        String str8_2 = in.next();
+        isStrangePair(str8_1,str8_2);
+
+
+        System.out.println("9: ");
+        String str9_1 = in.next();
+        String str9_2 = in.next();
+        if (str9_2.charAt(0)=='-')
+        {
+            isSuffix(str9_1, str9_2);
+        }
+        if (str9_2.charAt(str9_2.length()-1)=='-')
+        {
+            isPrefix(str9_1,str9_2);
+        }
     }
 
     public static void repeat(String str, int a)
@@ -58,6 +79,7 @@ public class SecondProg {
             System.out.print(str.charAt(i));
             }
         }
+        System.out.println();
     }
     public static void differenceMaxMin(int[] mas)
     {
@@ -74,7 +96,7 @@ public class SecondProg {
             }
         }
         result = max - min;
-        System.out.print(result);
+        System.out.println(result);
     }
 
     public static void isAvgWhole(int[] mas)
@@ -86,11 +108,11 @@ public class SecondProg {
         }
         if((sum%mas.length)==0)
         {
-            System.out.print("true");
+            System.out.println("true");
         }
         else
         {
-            System.out.print("false");
+            System.out.println("false");
         }
     }
 
@@ -107,6 +129,7 @@ public class SecondProg {
         {
             System.out.print(mas1[j]+" ");
         }
+        System.out.println();
     }
     public static void getDecimalPlaces(String str)
     {
@@ -118,7 +141,7 @@ public class SecondProg {
                 k= str.length()-i-1;
             }
         }
-        System.out.print(k);
+        System.out.println(k);
     }
 
     public static void fibonacci(int a)
@@ -130,6 +153,67 @@ public class SecondProg {
            n0=n1;
            n1=n2;
         }
-        System.out.print(n2);
+        System.out.println(n2);
+    }
+
+    public static void isValid(String str)
+    {
+        int k=0;
+        if (str.length()!=5)
+        {
+            k++;
+        }
+        for(int i=0; i<str.length();i++)
+        {
+            if(str.charAt(i)==' '|| Character.isLetter(str.charAt(i)))
+            {
+            k++;
+            }
+        }
+        if (k==0) {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+
+    public static void isStrangePair(String str1, String str2)
+    {
+        int k1 = str1.length();
+        int k2 = str2.length();
+        if (str1.charAt(0)==str2.charAt(k2-1) && str1.charAt(k1-1)==str2.charAt(0))
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+
+    public static void isSuffix (String str1, String str2)
+    {
+        int k=0;
+        for(int i=1; i<str2.length();i++)
+        {
+            while (str1.charAt(i-1)==str2.charAt(i))
+            {
+                return;
+            }
+            if (i==str2.length())
+            {
+                System.out.println("true");
+            }
+            else
+            {
+                System.out.println("false");
+            }
+        }
+    }
+    public static void isPrefix (String str1, String str2)
+    {
+
     }
 }
